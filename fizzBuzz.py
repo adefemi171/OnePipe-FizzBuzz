485a8dccbaa411cda40e965bff3889e66b206be5
Adefemi Micheal <adefemi171@gmail.com>
2019-12-28 21:03:59 -0500

    Update fizzBuzz.py

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,14 +1,26 @@
 def fizzBuzz():
     n = 100
-    for i in range(n):
-        # print (i)
-        if i % 3 == 0:
-            print('Fizz')
-        elif i % 5 == 0:
-            print('Buzz')
-        elif i % 3 == 0 and i % 5 == 0:
-            print('FizzBuzz')
+    for i in range(1, n + 1):
+        if i % 15 == 0:  
+            print("FizzBuzz")                                          
+            continue
+        elif i % 3 == 0:      
+            print("Fizz")                                          
+            continue
+        elif i % 5 == 0:          
+            print("Buzz")                                      
+            continue
         else:
-            print (i)
+            print(i)
+#     for i in range(n):
+#         # print (i)
+#         if i % 3 == 0:
+#             print('Fizz')
+#         elif i % 5 == 0:
+#             print('Buzz')
+#         elif i % 3 == 0 and i % 5 == 0:
+#             print('FizzBuzz')
+#         else:
+#             print (i)
 
 fizzBuzz()
